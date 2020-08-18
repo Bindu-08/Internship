262aa7032a40ed3f9d967721fe0ea133f3d7a40c
bindu sri sai <54704952+Bindusrisai@users.noreply.github.com>
2020-08-18 09:45:49 +0530

    Delete task.py
    commit

--- task.py
+++
@@ -1,7 +0,0 @@
-#Task5
-import pandas as pd
-import matplotlib as plt
-monthly=pd.read_csv(r'C:\Users\uppal\Desktop\documents\Data Science Final project\Dr. Semmelweis and the Discovery of Handwashing\datasets\monthly_deaths.csv',parse_dates=["date"])
-monthly["proportion_deaths"]=monthly["deaths"]/monthly["births"]
-ax = monthly.plot(x="date", y="proportion_deaths",label="plot")
-ax.set_ylabel("Proportion deaths")
